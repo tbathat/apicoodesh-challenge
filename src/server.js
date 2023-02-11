9020652c4a128d53d3991bf4edd9f90cbaea29e0
Tabatha Pradier <tabathapradier@gmail.com>
2023-02-11 19:24:21 -0300

    Refactor server setup for env and db connection

--- src/server.js
+++ src/server.js
@@ -1,7 +1,6 @@
-const api = require("./api");
+require('dotenv').config();
 const express = require("express");
-
-const  connectToDataBase = require("./db");
+const client = require("./db");
 
 const server = express();
 
@@ -9,7 +8,39 @@ server.use(express.json());
 server.listen(8081);
 
 //Rota padrão inicial solicitada no desafio
-server.get("/", (req, res) => {
-    return res.send ({ "HTTP 200": "Back-end Challenge 2021 🏅 - Space Flight News" });
+server.get("/", async (req, res) => {
+
+const db = client.db("spaceflightnews");
+const collection = db.collection('articles');
+
+const insertResult = await collection.insertMany([{
+    "id": 0,
+    "featured": false,
+    "title": "string",
+    "url": "string",
+    "imageUrl": "string",
+    "newsSite": "string",
+    "summary": "string",
+    "publishedAt": "string",
+    "launches": [
+      {
+        "id": "string",
+        "provider": "string"
+      }
+    ],
+    "events": [
+      {
+        "id": "string",
+        "provider": "string"
+      }
+    ]
+}]);
+console.log('Inserted documents =>', insertResult);
+
+// the following code examples can be pasted here...
+    return res.send ("Back-end Challenge 2021 🏅 - Space Flight News");
+
 });
+
+
 
