d6fbe95640f317231a1b8420d22f2892c9876c38
Tabatha Pradier <tabathapradier@gmail.com>
2023-02-12 12:40:59 -0300

    Simple update

--- src/server.js
+++ src/server.js
@@ -8,14 +8,14 @@ console.log("🚀 Server is running");
 server.use(express.json());
 server.listen(8081);
 
-//Rota padrão inicial solicitada no desafio
+//Retornar um Status: 200 e uma Mensagem "Back-end Challenge 2021 🏅 - Space Flight News
 server.get("/", async (req, res) => {
 
 const db = client.db("spaceflightnews");
 const collection = db.collection('articles');
 
 const insertResult = await collection.insertMany([{
-    "id": 0,
+    "id": "string",
     "featured": false,
     "title": "string",
     "url": "string",
@@ -35,8 +35,26 @@ const insertResult = await collection.insertMany([{
         "provider": "string"
       }
     ]
+
 }]);
 console.log('Inserted documents =>', insertResult);
+
+/*Listar todos os artigos da base de dados, utilizar o sistema de paginação para não sobrecarregar 
+a REQUEST -> [GET]/articles/: */
+server.get("/articles/:", (req, res) => {})
+
+//Obter a informação somente de um artigo -> [GET]/articles/{id}
+server.get("/articles/{id}", (req, res) => {});
+
+//Adicionar um novo artigo -> [PUT]/articles/:
+server.put("/articles/:", (req, res) => {});
+
+//Atualizar um artigo baseado no id -> [POST]/articles/{id}:
+server.post("/articles/{id}:", (req, res) => {});
+
+//Remover um artigo baseado no id -> [DELETE]/articles/{id}:
+server.delete("/articles/{id}:", (req, res) => {});
+
 
 // the following code examples can be pasted here...
     return res.send ("Back-end Challenge 2021 🏅 - Space Flight News");
